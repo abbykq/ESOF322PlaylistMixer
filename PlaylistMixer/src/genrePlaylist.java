cfb29c901b39d646e8e48f58efbe4f9aa5a69486
Trey Thompson <treythompson152@gmail.com>
2022-04-06 16:23:21 -0600

    Delete Again

--- PlaylistMixer/src/genrePlaylist.java
+++ PlaylistMixer/src/genrePlaylist.java
@@ -1,7 +1,4 @@
 public class genrePlaylist extends playlist{
     //This is a playlist that only contains one genre 
-    //Another comment
-    //Another another comment
-    //Another another another comment
-    
+
 }
