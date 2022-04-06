5fa3d2221e69e147a1bc38cc1f2b523d1dcb5a0f
Brady Underwood <underwood.brady@gmail.com>
2022-04-06 16:30:04 -0600

    removed comments

--- PlaylistMixer/src/genrePlaylist.java
+++ PlaylistMixer/src/genrePlaylist.java
@@ -1,8 +1,5 @@
 public class genrePlaylist extends playlist{
     //This is a playlist that only contains one genre 
-    //Another comment
-    //Another another comment
-    //Another another another comment
     
     public String getInfo(){
         return "";
