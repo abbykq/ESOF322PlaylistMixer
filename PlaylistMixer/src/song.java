5497457c21a0f81faba87478297c00ee74cc8acc
Brady Underwood <underwood.brady@gmail.com>
2022-04-06 12:29:27 -0600

    Added song class methods and attributes

--- PlaylistMixer/src/song.java
+++ PlaylistMixer/src/song.java
@@ -1,3 +1,17 @@
 public class song {
     //Stores title, genre, and artist to be stored in playlist
+    private final String title, artist, genre;
+
+    song(String t, String a,String g){
+        title = t;
+        artist = a;
+        genre = g;
+    }
+
+    public String getTitle(){ return title;}
+
+    public String getArtist(){ return artist;}
+
+    public String getGenre(){ return genre;}
+    
 }
