77dfbecdb8bce917058a88ef62cdd873d26fe529
Brady Underwood <underwood.brady@gmail.com>
2022-04-06 12:32:37 -0600

    added a space in constructor

--- PlaylistMixer/src/song.java
+++ PlaylistMixer/src/song.java
@@ -2,7 +2,7 @@ public class song {
     //Stores title, genre, and artist to be stored in playlist
     private final String title, artist, genre;
 
-    song(String t, String a,String g){
+    song(String t, String a, String g){
         title = t;
         artist = a;
         genre = g;
