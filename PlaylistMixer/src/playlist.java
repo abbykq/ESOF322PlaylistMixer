1b27950b5db90b09772b44bb290a62dd2effa4c1
Brady Underwood <underwood.brady@gmail.com>
2022-04-06 16:53:21 -0600

    Added outlined attributes to playlist.java

--- PlaylistMixer/src/playlist.java
+++ PlaylistMixer/src/playlist.java
@@ -1,3 +1,15 @@
+import java.util.Queue;
+import java.util.LinkedList;
+import java.util.ArrayList;
+
 public abstract class playlist {
     //Contains methods and attributes used in all playlist subclasses
+    private final Queue<song> songQueue = new LinkedList<song>();
+    private final ArrayList<song> songList = new ArrayList<song>();
+
+    private song currentSong;
+    private boolean isPlaying;
+
+    
+    
 }
