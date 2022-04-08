f209b9f70b148892f12ca1f3ca359ba7b4adeb4d
abbykq <abbykq@gmail.com>
2022-04-08 12:29:20 -0600

    added gui in main

--- PlaylistMixer/src/Main.java
+++ PlaylistMixer/src/Main.java
@@ -1,6 +1,19 @@
 public class Main {
     public static void main(String[] args){
-        System.out.println("Hello World :)");
+
+        System.out.println("Welcome to Playlist mixer");
+        System.out.println("Choose from the options below to continue");
+
+        System.out.println(" " +
+                "Play/Pause:           p\n " +
+                "Add Song to Playlist: a\n" +
+                " Remove Song:          r\n" +
+                " Add to Queue:         q\n" +
+                " Skip:                 n\n" +
+                " Shuffle:              s\n" +
+                " Get song info:        i");
+
+
     }
 }
 
