40496b1ee3d33edec7b2b1583829bdb2fdaacef2
abbykq <abbykq@gmail.com>
2022-04-18 15:45:06 -0600

    fixing up main

--- PlaylistMixer/src/Main.java
+++ PlaylistMixer/src/Main.java
@@ -27,15 +27,15 @@ public class Main {
         Song Utah = new Song("Utah", "Brotherkenzie", "Folk");
         Song MrMom = new Song("Mr. Mom", "Lonestar", "Country");
         Song SameDrugs = new Song("Same Drugs", "Chance The Rapper", "Rap");
-        // Song AroundTheWorld = new Song("Around the World", "Daft Punk", "EDM");
-        // Song ILoveYouSo= new Song("I Love You So", "The Walters", "Alt/Indie");
-        // Song BasketCase= new Song("Basket Case", "Green Day", "Punk Rock");
-        // Song MrBrightSide= new Song("Mr. Brightside", "The Killers", "Alt/Rock");
-        //Song BringMeToLife= new Song("Bring Me To Life", "Evanescence", "Metal");
-        //Song StairWayToHeaven= new Song("Stairway To Heaven", "Led Zeppelin", "Rock");
-        //Song ByAndBy= new Song("By and By", "Caamp", "Folk");
-        //Song LOVE= new Song("LOVE", "Nat King Cole", "Jazz");
-        //Song InternationalHarvester= new Song("International Harvester", "Craig Morgan", "Country");
+        Song AroundTheWorld = new Song("Around the World", "Daft Punk", "EDM");
+        Song ILoveYouSo = new Song("I Love You So", "The Walters", "Alt/Indie");
+        Song BasketCase = new Song("Basket Case", "Green Day", "Punk Rock");
+        Song MrBrightSide = new Song("Mr. Brightside", "The Killers", "Alt/Rock");
+        Song BringMeToLife = new Song("Bring Me To Life", "Evanescence", "Metal");
+        Song StairWayToHeaven = new Song("Stairway To Heaven", "Led Zeppelin", "Rock");
+        Song ByAndBy = new Song("By and By", "Caamp", "Folk");
+        Song LOVE = new Song("LOVE", "Nat King Cole", "Jazz");
+        Song InternationalHarvester = new Song("International Harvester", "Craig Morgan", "Country");
 
         List<Song> songs = new ArrayList<>();
         songs.add(Good4You);
@@ -58,6 +58,15 @@ public class Main {
         songs.add(Utah);
         songs.add(MrMom);
         songs.add(SameDrugs);
+        songs.add(AroundTheWorld);
+        songs.add(ILoveYouSo);
+        songs.add(BasketCase);
+        songs.add(MrBrightSide);
+        songs.add(BringMeToLife);
+        songs.add(StairWayToHeaven);
+        songs.add(ByAndBy);
+        songs.add(LOVE);
+        songs.add(InternationalHarvester);
         return songs;
     }
 
@@ -90,7 +99,7 @@ public class Main {
                 " Add to Queue:                queue\n" +
                 " Skip Current Song:           skip\n" +
                 " Shuffle Queue:               shuffle\n" +
-                " Get Playlist info:               info\n" +
+                " Get Playlist info:           info\n" +
                 " Print Playlist:              print\n" +
                 " Print Options again:         options\n" +
                 " End session:                 end\n");
@@ -104,43 +113,62 @@ public class Main {
 //        test3(songs);
 
         Playlist userPlaylist = null;
-
+        boolean correctInput = false;
         System.out.println("Welcome to Playlist mixer");
-        System.out.println("What type of playlist would you like to make? Choose from Basic, Genre, Artist: ");
-        Scanner playlistTypeInput = new Scanner(System.in);
-        String playlistType = playlistTypeInput.nextLine();
-        if (playlistType.equals("Basic")) {
-            userPlaylist = new basicPlaylist();
-        }
-        if (playlistType.equals("Genre")) {
-            Scanner genreInput = new Scanner(System.in);
-            System.out.println("What type of genre? ");
-            String genre = genreInput.nextLine();
-            userPlaylist = new genrePlaylist(genre);
-            List<Song> songs = songList();
-            for (Song songToCheck : songs) {
-                Song checkGenre = (Song) songToCheck;
-                String songGenre = checkGenre.getGenre();
-                if (songGenre.equals(genre)) {
-                    userPlaylist.addSong(songToCheck);
+
+        do {
+            System.out.println("What type of playlist would you like to make? Choose from Basic, Genre, Artist: ");
+            Scanner playlistTypeInput = new Scanner(System.in);
+            String playlistType = playlistTypeInput.nextLine();
+            playlistType = playlistType.toLowerCase();
+
+            if (playlistType.equals("basic")) {
+                userPlaylist = new basicPlaylist();
+                correctInput = true;
+            } else if (playlistType.equals("genre")) {
+                Scanner genreInput = new Scanner(System.in);
+                String genre;
+                ArrayList<String> genreList = new ArrayList<>(List.of("pop", "alt/indie", "jazz", "edm", "punk rock", "alt/rock", "metal", "rock", "folk", "country", "rap"));
+                do {
+                    System.out.println("Select type of genre: ");
+                    System.out.println("Pop, Alt/Indie, Jazz, EDM, Punk Rock, Alt/Rock, Metal, Rock, Folk, Country, Rap");
+                    genre = genreInput.nextLine();
+                    genre = genre.toLowerCase().strip();
+                    if(!genreList.contains(genre)){
+                        System.out.println("This is not a recognized genre");
+                    }
+                } while(!genreList.contains(genre));
+                genre = genre.substring(0,1).toUpperCase() + genre.substring(1);
+                userPlaylist = new genrePlaylist(genre);
+                List<Song> songs = songList();
+                for (Song songToCheck : songs) {
+                    Song checkGenre = (Song) songToCheck;
+                    String songGenre = checkGenre.getGenre();
+                    if (songGenre.equals(genre)) {
+                        userPlaylist.addSong(songToCheck);
+                    }
                 }
+                correctInput = true;
+            } else if (playlistType.equals("artist")) {
+                Scanner artistInput = new Scanner(System.in);
+                System.out.println("What artist? ");
+                String artist = artistInput.nextLine();
+                userPlaylist = new artistPlaylist(artist);
+                List<Song> songs = songList();
+                for (Song songToCheck : songs) {
+                    Song checkArtist = (Song) songToCheck;
+                    String songArtist = checkArtist.getArtist();
+                    if (songArtist.equals(artist)) {
+                        userPlaylist.addSong(songToCheck);
+                    }
+                }
+                correctInput = true;
+            } else {
+                System.out.println("Incorrect input, please try again");
             }
 
+        }while(!correctInput);
 
-        }
-        if (playlistType.equals("Artist")) {
-            Scanner artistInput = new Scanner(System.in);
-            System.out.println("What artist? ");
-            String artist = artistInput.nextLine();
-            userPlaylist = new artistPlaylist(artist);
-            List<Song> songs = songList();
-            for (Song songToCheck : songs) {
-                Song checkArtist = (Song) songToCheck;
-                String songArtist = checkArtist.getGenre();
-                if (songArtist.equals(artist)) {
-                    userPlaylist.addSong(songToCheck);
-                }
-            }
 
             System.out.println("Choose from the options below to continue");
 
@@ -151,14 +179,16 @@ public class Main {
             do {
                 System.out.print("Enter selection: ");
                 selection = input.nextLine();
-                // TODO add if selection = "a"...
+                selection = selection.toLowerCase();
                 if (selection.equals("play") || selection.equals("pause")) {
                     userPlaylist.togglePlayPause();
+                    System.out.println("type \'help\' to print options");
                 } else if (selection.equals("play song")) {
                     Scanner toPlayInput = new Scanner(System.in);
                     System.out.print("Which song? ");
                     String toPlay = toPlayInput.nextLine();
                     userPlaylist.playSong(toPlay);
+                    System.out.println("type \'help\' to print options");
                 } else if (selection.equals("add")) {
                     Scanner songTitleInput = new Scanner(System.in);
                     Scanner songArtistInput = new Scanner(System.in);
@@ -171,31 +201,44 @@ public class Main {
                     String genre = songGenreInput.nextLine();
                     Song newSong = new Song(title, artistAdd, genre);
                     userPlaylist.addSong(newSong);
+                    System.out.println("Type \'help\' to print options");
                 } else if (selection.equals("remove")) {
                     Scanner toRemoveInput = new Scanner(System.in);
                     System.out.print("Song to remove: ");
                     String toRemove = toRemoveInput.nextLine();
                     userPlaylist.removeSong(toRemove);
+                    System.out.println("Type \'help\' to print options");
                 } else if (selection.equals("queue")) {
                     Scanner toQueueInput = new Scanner(System.in);
                     System.out.print("Song to queue: ");
                     String toQueue = toQueueInput.nextLine();
                     userPlaylist.queueSong(toQueue);
+                    System.out.println("Type \'help\' to print options");
                 } else if (selection.equals("skip")) {
                     userPlaylist.skip();
+                    System.out.println("Type \'help\' to print options");
                 } else if (selection.equals("shuffle")) {
                     userPlaylist.shuffle();
+                    System.out.println("Type \'help\' to print options");
                 } else if (selection.equals("info")) {
                     System.out.println(userPlaylist.getInfo());
+                    System.out.println("Type \'help\' to print options");
                 } else if (selection.equals("print")) {
                     userPlaylist.print();
-                } else if (selection.equals("options")) {
+                    System.out.println("Type \'help\' to print options");
+                } else if (selection.equals("help")) {
                     printOptions();
+                    System.out.println("Type \'help\' to print options");
+                } else if (selection.equals("end")){
+                    System.out.println("Thanks for using PlayMix Mixa!");
+                } else{
+                    System.out.println("Incorrect input, please try again");
                 }
+
             } while (!selection.equals("end"));
 
             System.exit(0);
         }
     }
-}
+
 
