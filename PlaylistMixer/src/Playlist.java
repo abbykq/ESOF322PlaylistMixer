c1abda9120d8016df3734d6e793cac87e18d2d51
Trey Thompson <treythompson152@gmail.com>
2022-04-18 11:47:54 -0600

    Working Methods

--- PlaylistMixer/src/Playlist.java
+++ PlaylistMixer/src/Playlist.java
@@ -3,6 +3,7 @@ import java.util.Queue;
  import java.util.List;
  import java.util.ArrayList;
  import java.util.Collections;
+ import java.util.Arrays;
 
  public abstract class Playlist {
      //Contains methods and attributes used in all playlist subclasses
@@ -13,13 +14,23 @@ import java.util.Queue;
 
      private boolean isPlaying;
 
+     public void print(){
+        System.out.println("PLAYLIST:");
+        for(Song s:songList){
+            System.out.println(
+            s.getTitle() +
+            " by " + s.getArtist() +
+            " -- " +
+            " Genre: " + s.getGenre());
+        }
+     }
      public void addSong(Song s){
-         // Trey 
          if(songList.contains(s)){
-             System.out.println("The song, \"" + s.getGenre() + "\", is already in the playlist");
+            System.out.println("The song \"" + s.getTitle() + "\" by " + s.getArtist() + " is already in your playlist");
          }
          else{
              songList.add(s);
+             System.out.println("The song \"" + s.getTitle() + "\" by " + s.getArtist() + " is now in your playlist");
          }
 
      }
@@ -30,22 +41,24 @@ import java.util.Queue;
             if(iter.getTitle().equals(s)){
                System.out.println("The song \"" + iter.getTitle() + "\" by " + iter.getArtist() + " is removed from the playlist");
                songList.remove(i);
-               break;
+               return;
             }
-            System.out.println("This song is not in your playlist");
         }
+        System.out.println("This song is not in your playlist");
      }
 
      public void playSong(String songName){
+         
          for(int i = 0; i < songList.size(); i++){
              Song iter = songList.get(i);
              if(iter.getTitle().equals(songName)){
                 System.out.println("The song \"" + iter.getTitle() + "\" by " + iter.getArtist() + " is now playing");
                 currentSong = iter;
-                break;
+                isPlaying = true;
+                return;
              }
-
          }
+        System.out.println("Could not find song in playlist");
      }
 
      public void queueSong(String songName){
@@ -53,7 +66,8 @@ import java.util.Queue;
          for(Song Song: songList){
              if (Song.getTitle().equals(songName)){
                  songQueue.add(Song);
-                 break;
+                 System.out.println("The song has been added to the queue");
+                 return;
              }
          }
          System.out.println("ERROR: Didn't find song to queue");
@@ -61,30 +75,53 @@ import java.util.Queue;
 
      public void togglePlayPause(){
          isPlaying = !isPlaying;
+         if(isPlaying == false){
+            System.out.println("Paused: \"" + currentSong.getTitle() + "\" by " + currentSong.getArtist());
+         }
+         else{
+            System.out.println("Playing: \"" + currentSong.getTitle() + "\" by " + currentSong.getArtist());
+         }
      }
 
      public void skip(){
-         songQueue.remove();
+        if(isPlaying){
+            System.out.println("No longer playing...\"" + currentSong.getTitle() + "\" by " + currentSong.getArtist());
+        }
+        else{
+            System.out.println("There is no song playing!");
+            return;
+        }
+
+        if(songQueue.isEmpty()){
+            System.out.println("Your queue is empty!");
+            isPlaying = false;
+            return;
+        } 
+        currentSong = songQueue.remove();
 
          //If there isn't another item in queue then stop
-         if(songQueue.peek() == null){
-             isPlaying = false;
-             return;
-         }
-            
-        currentSong = songQueue.peek();
+
+        System.out.println("Now playing...\"" + currentSong.getTitle() + "\" by " + currentSong.getArtist());
      }
 
      public void shuffle(){
+        System.out.println("Shuffling...");
+        Song sa[] = new Song[songList.size()];
+        sa = songList.toArray(sa);
+
+        songQueue.clear(); // Clears the previous queue
+        
+        
          //Converts songQueue to List and shuffles
-         List<Song> sList = (List) songQueue;
+         List<Song> sList = Arrays.asList(sa);
          Collections.shuffle(sList);
 
          //Clears playlist and adds shuffled songs
-         songQueue.clear();
+         
          for(Song s: sList){
              songQueue.add(s);
          }
+         this.skip();
      }
 
      public abstract String getInfo();
