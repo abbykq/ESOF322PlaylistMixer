23938268e2c200da900a7ad9b753484d703d61be
Trey Thompson <treythompson152@gmail.com>
2022-04-18 16:00:17 -0600

    fixed queue song

--- PlaylistMixer/src/Playlist.java
+++ PlaylistMixer/src/Playlist.java
@@ -62,7 +62,7 @@ import java.util.Queue;
      }
 
      public void queueSong(String songName){
-        if(!isPlaying){ // If there is not a song playing, that queued song plays the queued song
+        if(!isPlaying && currentSong == null){ // If there is not a song playing, that queued song plays the queued song
             this.playSong(songName);
             return;
         }
